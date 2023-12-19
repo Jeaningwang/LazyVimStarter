6ffd096155c91a71f041b01a20b06528267e09c3
dong.wang <moneatts@outlook.com>
2023-12-19 14:44:12 +0800

    feat(添加 keymaps, options 的配置)

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,98 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+-- define common options
+local opts = {
+    noremap = true, -- non-recursive
+    silent = true,  -- do not show message
+}
+
+-- 保证编辑的行一直居中
+vim.keymap.set({ 'n' }, '#', '#zz', opts)
+vim.keymap.set({ 'n' }, '*', '*zz', opts)
+vim.keymap.set({ 'n' }, 'w', 'wzz', opts)
+vim.keymap.set({ 'n' }, 'W', 'Wzz', opts)
+vim.keymap.set({ 'n' }, 'e', 'ezz', opts)
+vim.keymap.set({ 'n' }, 'E', 'Ezz', opts)
+vim.keymap.set({ 'n' }, 'b', 'bzz', opts)
+vim.keymap.set({ 'n' }, 'B', 'Bzz', opts)
+vim.keymap.set({ 'n' }, 'H', 'Hzz', opts)
+vim.keymap.set({ 'n' }, 'L', 'Lzz', opts)
+vim.keymap.set({ 'n' }, '$', '$zz', opts)
+vim.keymap.set({ 'n' }, '^', '^zz', opts)
+vim.keymap.set({ 'n' }, 'j', 'jzz', opts)
+vim.keymap.set({ 'n' }, 'k', 'kzz', opts)
+vim.keymap.set({ 'n' }, 'G', 'Gzz', opts)
+vim.keymap.set({ 'n' }, 'u', 'uzz', opts)
+vim.keymap.set({ 'n' }, '(', '(zz', opts)
+vim.keymap.set({ 'n' }, ')', ')zz', opts)
+vim.keymap.set({ 'n' }, '{', '{zz', opts)
+vim.keymap.set({ 'n' }, '}', '}zz', opts)
+vim.keymap.set({ 'n' }, '[{', '[{zz', opts)
+vim.keymap.set({ 'n' }, ']}', ']}zz', opts)
+vim.keymap.set({ 'n' }, 'gd', 'gdzz', opts)
+
+-----------------
+-- Normal mode --
+-----------------
+
+-- Hint: see `:h vim.map.set()`
+-- Better window navigation
+vim.keymap.set('n', '<C-h>', '<C-w>h', opts)
+vim.keymap.set('n', '<C-j>', '<C-w>j', opts)
+vim.keymap.set('n', '<C-k>', '<C-w>k', opts)
+vim.keymap.set('n', '<C-l>', '<C-w>l', opts)
+vim.keymap.set('n', '<leader>ww', '<C-w>w', opts)
+
+-- redo
+vim.keymap.set('n', 'U', '<C-r>', opts)
+
+-- Resize with arrows
+-- delta: 2 lines
+vim.keymap.set('n', '<C-Up>', ':resize -2<CR>', opts)
+vim.keymap.set('n', '<C-Down>', ':resize +2<CR>', opts)
+vim.keymap.set('n', '<C-Left>', ':vertical resize -2<CR>', opts)
+vim.keymap.set('n', '<C-Right>', ':vertical resize +2<CR>', opts)
+
+-- 设置跳转到行的头部或尾部
+vim.keymap.set({ 'n', 'v' }, 'gh', '^', opts)
+vim.keymap.set({ 'n', 'v' }, 'gl', '$', opts)
+
+-- 返回上一处和下一处修改点
+vim.keymap.set('n', '<A-Left>', '<C-o>', opts)
+vim.keymap.set('n', '<A-Right>', '<C-i>', opts)
+
+-- 向上/向下翻页并居中
+vim.keymap.set('n', '<leader>h', '<S-h>zz', opts)
+vim.keymap.set('n', '<leader>l', '<S-l>zz', opts)
+
+-----------------
+-- Visual mode --
+-----------------
+
+-- Hint: start visual mode with the same area as the previous area and the same mode
+vim.keymap.set('v', '<', '<gv', opts)
+vim.keymap.set('v', '>', '>gv', opts)
+
+-- 退出可视模式
+vim.keymap.set('v', 'v', '<ESC>', opts)
+
+
+-----------------
+-- Insert mode --
+-----------------
+
+vim.keymap.set('i', 'jk', '<ESC>', opts)
+
+
+-----------------
+-- Others --
+-----------------
+-- 禁用掉方向键
+vim.keymap.set({ 'i', 'n' }, '<Left>', '<Nop>', opts)
+vim.keymap.set({ 'i', 'n' }, '<Right>', '<Nop>', opts)
+vim.keymap.set({ 'i', 'n' }, '<Up>', '<Nop>', opts)
+vim.keymap.set({ 'i', 'n' }, '<Down>', '<Nop>', opts)
+-- 禁用掉ESC
+vim.keymap.set({ 'i', 'n' }, '<Esc>', '<Nop>', opts)
--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,3 +1,61 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+-- Hint: use `:h <option>` to figure out the meaning if needed
+vim.opt.clipboard = "unnamedplus" -- use system clipboard
+vim.opt.completeopt = { "menu", "menuone", "noselect" }
+vim.opt.mouse = "a" -- allow the mouse to be used in Nvim
+
+-- Tab
+vim.opt.tabstop = 4 -- number of visual spaces per TAB
+vim.opt.softtabstop = 4 -- number of spacesin tab when editing
+vim.opt.shiftwidth = 4 -- insert 4 spaces on a tab
+vim.opt.expandtab = true -- tabs are spaces, mainly because of python
+
+-- UI config
+-- 设置字体字号
+-- vim.cmd("set guifont=" .. vim.fn.fnameescape("Source Code Pro") .. ":h14")
+vim.cmd("set guifont=" .. vim.fn.fnameescape("SauceCodePro Nerd Font Mono") .. ":h16")
+-- vim.cmd('set guifont=' .. vim.fn.fnameescape('MesloLGL Nerd Font Mono') .. ':h14')
+-- vim.cmd('set guifont=' .. vim.fn.fnameescape('Hack Nerd Font Mono') .. ':h14')
+vim.opt.number = true -- show absolute number
+vim.opt.relativenumber = true -- add numbers to each line on the left side
+vim.opt.cursorline = true -- highlight cursor line underneath the cursor horizontally
+vim.opt.splitbelow = true -- open new vertical split bottom
+vim.opt.splitright = true -- open new horizontal splits right
+vim.opt.termguicolors = true -- enabl 24-bit RGB color in the TUI
+vim.opt.showmode = false -- we are experienced, wo don't need the "-- INSERT --" mode hint
+-- 光标移动的时候始终保持上下左右至少有 8 个空格的间隔
+vim.o.scrolloff = 4
+vim.o.sidescrolloff = 4
+
+-- Searching
+vim.opt.incsearch = true -- search as characters are entered
+vim.opt.hlsearch = false -- do not highlight matches
+vim.opt.ignorecase = true -- ignore case in searches by default
+vim.opt.smartcase = true -- but make it case sensitive if an uppercase is entered
+
+-- Ohters
+-- 使用 utf-8 编码
+vim.g.encoding = "utf-8"
+vim.o.fileencoding = "utf-8"
+-- 设置 leader
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+-- 禁止创建备份文件
+vim.o.backup = false
+vim.o.writebackup = false
+vim.o.swapfile = false
+-- 把空格显示成一个点
+vim.o.list = true
+vim.o.listchars = "space:·"
+-- 右侧参考线
+vim.wo.colorcolumn = "120"
+-- 新行对齐当前行
+vim.bo.autoindent = true
+vim.bo.smartindent = true
+-- 设置命令行的高度
+vim.o.cmdheight = 1
+-- 设置自动换行
+vim.o.wrap = 1
